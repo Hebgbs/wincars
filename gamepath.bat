ea66e672e0c9ab5be5185cb9b76175a35870daab
dw-hb <42311622+dw-hb@users.noreply.github.com>
2019-04-05 02:51:58 -0400

    Messages and other optimizations.

--- gamepath.bat
+++ gamepath.bat
@@ -14,8 +14,11 @@ REM | 32-bit Program files check; Only important for people who installed retail
 IF EXIST "%programfiles(x86)%" SET prgm="%programfiles(x86)%"
 IF NOT EXIST "%programfiles(x86)%" SET prgm="%programfiles%"
 
+REM | !!! Nothing else above matters, what's below should be what you're minding.
 SET gamedir=%prgm%\Acclaim\revolt
-EXIT /b
 
 REM | If filechk.bat is giving you trouble about game path not being correct,
 REM | then edit the set command above so %gamedir% points to where your game is!
+
+REM | !!! Don't touch this. No, I'm serious, you'll break stuff.
+EXIT /b
